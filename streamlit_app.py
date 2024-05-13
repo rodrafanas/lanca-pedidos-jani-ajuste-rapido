ae81fd1c128abdc0f95258c9669a9f449aa88f80
Rafael Rodrigues do Nascimento <rodrafanas@gmail.com>
2024-05-13 16:14:38 -0300

    Adicionando tabela de pedidos ... in progress.

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,83 @@ st.title("Emissão de Pedidos de Reformas de Roupas")
 nome_cliente = st.text_input("Nome do Cliente")
 telefone = st.text_input("Telefone")
 pedido = st.text_input("Pedido")
+
+
+# options = st.multiselect(
+#     "Escolha as opções abaixo?",
+#     ["Barra de calça simples", "Barra de calça jeans original", "Barra de vestido de festa", "Troca de zíper simples", "Troca de zíper de jeans", "Barra de calça simples", "Barra de calça jeans original", "Barra de vestido de festa", "Troca de zíper simples", "Troca de zíper de jeans", "Barra de calça simples", "Barra de calça jeans original", "Barra de vestido de festa", "Troca de zíper simples", "Troca de zíper de jeans"],
+#     None)
+
+# # Barra de calça 30 simples 
+# # Barra de calça jeans original 40
+# # Barra de vestido de festa 120
+
+# # Troca de zíper de jeans 40
+# # Troca de zíper simples 30
+
+
+# st.write("Voce selecionou:", options)
+
+# edited_df = st.data_editor(options)
+
+# pedido = 
+
+######################################
+import streamlit as st
+import pandas as pd
+
+# Dicionário com os preços de cada serviço
+precos = {
+    "Barra de calça simples": 30,
+    "Barra de calça jeans original": 40,
+    "Barra de vestido de festa": 120,
+    "Troca de zíper simples": 30,
+    "Troca de zíper de jeans": 40
+}
+
+# # Função para calcular o preço total com base nas seleções e quantidades
+# def calcular_preco_total(df):
+#     total = 0
+#     for index, row in df.iterrows():
+#         preco_unitario = precos[row["Serviço"]]
+#         quantidade = row["Quantidade"]
+#         total += preco_unitario * quantidade
+#     return total
+
+# Função para calcular o preço total com base nas seleções e quantidades
+def calcular_preco_total(df):
+    total = 0
+    for i in range(len(df)):
+        servico = df.iloc[i]["Serviço"]
+        quantidade = df.iloc[i]["Quantidade"]
+        preco_unitario = precos[servico]
+        total += preco_unitario * quantidade
+    return total
+
+# Seleção das opções
+options = st.multiselect(
+    "Escolha as opções abaixo:",
+    list(precos.keys()),
+    None
+)
+
+# Criando DataFrame com as opções selecionadas, quantidade padrão igual a 1 e o preço de cada serviço
+df = pd.DataFrame({
+    "Serviço": options,
+    "Quantidade": [1] * len(options),
+    "Preço": [precos[option] for option in options]
+})
+
+# Permitindo ao usuário editar as quantidades
+edited_df = st.dataframe(df)
+
+# Calculando o preço total
+# preco_total = calcular_preco_total(edited_df.data)
+# st.write("Preço Total:", preco_total)
+
+
+
+
 data_pedido = st.date_input("Data do Pedido")
 data_entrega = st.date_input("Data de Entrega")
 valor_pedido = st.number_input("Valor do Pedido (R$)", format="%.2f")
